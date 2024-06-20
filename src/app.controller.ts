a6ee3e4528ce03eda4876063e77a04fbd707b336
RamyBadr <eng.ramybadr66@gmail.com>
2024-06-20 19:02:48 +0300

    Revert "add todo"
    This reverts commit 629acd06ea53a368c198969473a75f5eb12a209f.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-//TODO: remove unused endpoints
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
